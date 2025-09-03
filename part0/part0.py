8be512d58fadaa828eee0830dba0f1ea8eee718a
delsqrdpsi <sbsaminenisai@gmail.com>
2025-09-03 15:19:44 -0500

    part0 problem2

--- part0/part0.py
+++ part0/part0.py
@@ -2,22 +2,21 @@
 # 09/03/2025
 # Part 0 - Speeding ticket
 
-def part0_speeding_ticket():
-    speed_limit = int(input())
-    speed_driven = int(input())
+# def part0_speeding_ticket():
+#     speed_limit = int(input())
+#     speed_driven = int(input())
 
-    if speed_driven <= speed_limit-10:
-        print(50)
-    elif (speed_driven - speed_limit) >= 6 and (speed_driven - speed_limit) <= 20:
-        print(75)
-    elif (speed_driven - speed_limit) >= 21 and (speed_driven - speed_limit) <= 40:
-        print(150)
-    elif speed_driven - speed_limit > 40:
-        print(300)
-    else:
-        print(0)
+#     if speed_driven <= speed_limit-10:
+#         print(50)
+#     elif (speed_driven - speed_limit) >= 6 and (speed_driven - speed_limit) <= 20:
+#         print(75)
+#     elif (speed_driven - speed_limit) >= 21 and (speed_driven - speed_limit) <= 40:
+#         print(150)
+#     elif speed_driven - speed_limit > 40:
+#         print(300)
+#     else:
+#         print(0)
 
-part0_speeding_ticket()
 
 # test cases
 # 35 45 -> 75
@@ -26,4 +25,26 @@ part0_speeding_ticket()
 # 35 76 -> 300
 # 35 0 -> 50
 
+def part0_heating_cooling_days():
+    temp = 0
+    heating_days = 0
+    cooling_days = 0
 
+    while temp > -459:
+        try:
+            temp = int(input("Enter the average daily temperature: "))
+        except ValueError:
+            print("use an integer stupid")
+            continue
+        if temp < 60 and temp >= -459:
+            heating_days += 1
+        elif temp > 80:
+            cooling_days += 1
+        
+    print("Heating days:",heating_days)
+    print("Cooling days:",cooling_days)
+
+
+part0_heating_cooling_days()
+
+# test cases
